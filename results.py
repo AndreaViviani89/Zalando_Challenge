100478d72aa78c6d5f7b9ac0c6d3d9c2a493ed29
Andrea Viviani <andrea.viviani89@gmail.com>
2022-05-31 16:37:48 +0200

    update

--- results.py
+++ results.py
@@ -162,3 +162,125 @@ Epoch: 30/30
 Accuracy = 89.9800033569336
 Training time: 1172.6960680484772
 '''
+
+# working on train_final
+'''Epoch: 1/30
+Accuracy = 84.11000061035156
+Epoch: 2/30
+Accuracy = 86.44999694824219
+Epoch: 3/30
+Accuracy = 86.16999816894531
+Epoch: 4/30
+Accuracy = 86.69999694824219
+Epoch: 5/30
+Accuracy = 87.33999633789062
+Epoch: 6/30
+Accuracy = 87.62000274658203
+Epoch: 7/30
+Accuracy = 88.52999877929688
+Epoch: 8/30
+Accuracy = 88.25
+Epoch: 9/30
+Accuracy = 88.66999816894531
+Epoch: 10/30
+Accuracy = 88.26000213623047
+Epoch: 11/30
+Accuracy = 88.83000183105469
+Epoch: 12/30
+Accuracy = 88.83999633789062
+Epoch: 13/30
+Accuracy = 89.2300033569336
+Epoch: 14/30
+Accuracy = 88.94999694824219
+Epoch: 15/30
+Accuracy = 89.48999786376953
+Epoch: 16/30
+Accuracy = 89.5
+Epoch: 17/30
+Accuracy = 89.31999969482422
+Epoch: 18/30
+Accuracy = 89.56999969482422
+Epoch: 19/30
+Accuracy = 89.47000122070312
+Epoch: 20/30
+Accuracy = 89.5
+Epoch: 21/30
+Accuracy = 89.98999786376953
+Epoch: 22/30
+Accuracy = 89.75
+Epoch: 23/30
+Accuracy = 90.12000274658203
+Epoch: 24/30
+Accuracy = 89.81999969482422
+Epoch: 25/30
+Accuracy = 89.5199966430664
+Epoch: 26/30
+Accuracy = 90.13999938964844
+Epoch: 27/30
+Accuracy = 90.05999755859375
+Epoch: 28/30
+Accuracy = 89.87999725341797
+'''
+
+# with lr 0,001
+'''Epoch: 1/30
+Accuracy = 85.05999755859375
+Epoch: 2/30
+Accuracy = 86.37999725341797
+Epoch: 3/30
+Accuracy = 86.83999633789062
+Epoch: 4/30
+Accuracy = 87.54000091552734
+Epoch: 5/30
+Accuracy = 87.87999725341797
+Epoch: 6/30
+Accuracy = 87.26000213623047
+Epoch: 7/30
+Accuracy = 88.26000213623047
+Epoch: 8/30
+Accuracy = 88.30000305175781
+Epoch: 9/30
+Accuracy = 88.75
+Epoch: 10/30
+Accuracy = 88.75
+Epoch: 11/30
+Accuracy = 88.83999633789062
+Epoch: 12/30
+Accuracy = 89.30000305175781
+Epoch: 13/30
+Accuracy = 89.38999938964844
+Epoch: 14/30
+Accuracy = 88.94000244140625
+Epoch: 15/30
+Accuracy = 89.47000122070312
+Epoch: 16/30
+Accuracy = 89.73999786376953
+Epoch: 17/30
+Accuracy = 89.98999786376953
+Epoch: 18/30
+Accuracy = 89.72000122070312
+Epoch: 19/30
+Accuracy = 89.63999938964844
+Epoch: 20/30
+Accuracy = 89.62000274658203
+Epoch: 21/30
+Accuracy = 89.81999969482422
+Epoch: 22/30
+Accuracy = 89.87000274658203
+Epoch: 23/30
+Accuracy = 89.7300033569336
+Epoch: 24/30
+Accuracy = 89.98999786376953
+Epoch: 25/30
+Accuracy = 90.0199966430664
+Epoch: 26/30
+Accuracy = 90.1500015258789
+Epoch: 27/30
+Accuracy = 90.27999877929688
+Epoch: 28/30
+Accuracy = 89.9800033569336
+Epoch: 29/30
+Accuracy = 90.29000091552734
+Epoch: 30/30
+Accuracy = 90.06999969482422
+Training time: 1246.0962190628052'''
