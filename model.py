7db62c6c32984efc220bb740bd9fe8924af71a3c
Andrea Viviani <andrea.viviani89@gmail.com>
2022-05-31 12:14:43 +0200

    Update data_handler and model file

---
+++ model.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+import torch
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+
+from collections import OrderedDict
